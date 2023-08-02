88ff57af7909ad814514e6deba9d44936edef73e
MatiCordoba <cordobaamatias@gmail.com>
2023-08-02 18:02:12 -0300

    final app node

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('node:path');
 
+const indexRoutes = require('./routes/index');
+
 const app = express()
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
+app.use(indexRoutes);
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000)
 console.log('Server is listening on port', 3000)
---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'First Web with Node',
+    })
+})
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+    })
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+        title: 'Contact Page',
+    })
+})
+
+module.exports = router
